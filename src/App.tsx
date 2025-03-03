e28a1fdbc5ec76d6d3e73da3cb609acc64e14dc0
Jason Hung <brook110413@gmail.com>
2025-03-03 13:28:50 +0800

    style: refactor style management and update import paths
    - Consolidate style imports in `App.tsx` by removing `GlobalStyle` and keeping only two styled components.
    - Remove the `GlobalStyle` component from `App.tsx`.
    - Update import paths to use absolute imports in `main.tsx`.
    - Add `GlobalStyle` import and usage in `main.tsx`.
    
    Signed-off-by: Jason Hung <brook110413@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import { useEffect } from 'react';
 import { JoinClassroomCard, StudentListCard } from '@/compositions';
 import { useAppDispatch } from '@/redux/store';
 import { fetchClassroom } from '@/redux/slices/classroomSlice';
-import {
-  GlobalStyle,
-  StyledAppBackground,
-  StyledClassroomContainer,
-} from '@/styles';
+import { StyledAppBackground, StyledClassroomContainer } from '@/styles';
 
 function App() {
   const dispatch = useAppDispatch();
@@ -16,15 +12,12 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      <GlobalStyle />
-      <StyledAppBackground>
-        <StyledClassroomContainer>
-          <JoinClassroomCard />
-          <StudentListCard />
-        </StyledClassroomContainer>
-      </StyledAppBackground>
-    </>
+    <StyledAppBackground>
+      <StyledClassroomContainer>
+        <JoinClassroomCard />
+        <StudentListCard />
+      </StyledClassroomContainer>
+    </StyledAppBackground>
   );
 }
 
