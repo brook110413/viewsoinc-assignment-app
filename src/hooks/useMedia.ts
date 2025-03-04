514cc1b73365ed0748a2031ee47db0cfe620e9b0
Jason Hung <brook110413@gmail.com>
2025-03-04 12:25:17 +0800

    style: use absolute imports for breakpoints
    - Update import path for breakpoints to use absolute imports.
    
    Signed-off-by: Jason Hung <brook110413@gmail.com>

--- src/hooks/useMedia.ts
+++ src/hooks/useMedia.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { breakpoints } from '../styles/breakpoints';
+import { breakpoints } from '@/styles/breakpoints';
 
 type Media = keyof typeof breakpoints;
 
